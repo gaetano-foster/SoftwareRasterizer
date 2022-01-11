f39165d4b17c5710857bdeb48a15d4c833a4f29b
Gaetano Foster <gaetanox109@gmail.com>
2022-01-11 10:52:26 -0500

    Changed resolution

--- src/Main.cpp
+++ src/Main.cpp
@@ -162,7 +162,7 @@ private:
 int main(int argc, char **argv)
 {
 	Rasterizer demo;
-	if (demo.Construct(SCREEN_WIDTH, SCREEN_HEIGHT, 1, 1))
+	if (demo.Construct(SCREEN_WIDTH, SCREEN_HEIGHT, 4, 4))
 		demo.Start();
 
 	return 0;
