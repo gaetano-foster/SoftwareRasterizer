6c790fa6a84ed0aedac4721906c3fb88f75236c7
Gaetano Foster <gaetanox109@gmail.com>
2022-01-14 18:29:04 -0500

    Put pixel size in Definitions.h

--- src/Definitions.h
+++ src/Definitions.h
@@ -5,6 +5,7 @@
 
 #define SCREEN_WIDTH        256
 #define SCREEN_HEIGHT       240
+#define PX_SIZE             4
 #define ASPECT_RATIO        (float)(SCREEN_HEIGHT / (float)SCREEN_WIDTH)
 
 #endif
--- src/main/Main.cc
+++ src/main/Main.cc
@@ -4,7 +4,7 @@
 int main(int argc, char **argv)
 {
 	Rasterizer demo;
-	if (demo.Construct(SCREEN_WIDTH, SCREEN_HEIGHT, 4, 4))
+	if (demo.Construct(SCREEN_WIDTH, SCREEN_HEIGHT, PX_SIZE, PX_SIZE))
 		demo.Start();
 
 	return 0;
