61c526d72748243fa36682ab2c878dfc47758050
Gaetano Foster <gaetanox109@gmail.com>
2022-01-13 19:17:25 -0500

    Moved Rasterizer class implementation to its own file, and its definition to Engine.h.

---
+++ src/engine/Rasterizer.cpp
@@ -0,0 +1,43 @@
+#include "Engine.h"
+
+Rasterizer::Rasterizer() 
+{
+	sAppName = "SoftwareRasterizer";
+}
+
+bool Rasterizer::OnUserCreate()
+{
+    vCamera = { 0, 0, 0 };
+    matProj.SetProjectionMatrix(0.1f, 1000.0f, 90.0f);
+    eCube.mesh = {
+	    // SOUTH
+	    { 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
+	    { 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+	    // EAST                                                      
+	    { 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
+	    { 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
+	    // NORTH                                                     
+	    { 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
+	    { 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
+	    // WEST                                                      
+	    { 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
+	    { 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
+	    // TOP                                                       
+	    { 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
+	    { 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
+	    // BOTTOM                                                    
+	    { 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
+	    { 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+    };
+	eCube.z = 3;
+	
+	return true;
+}
+
+bool Rasterizer::OnUserUpdate(float fElapsedTime)
+{
+    Clear(olc::BLACK);
+	eCube.Update();
+	eCube.Render(vCamera, matProj, this);
+	return true;
+}
