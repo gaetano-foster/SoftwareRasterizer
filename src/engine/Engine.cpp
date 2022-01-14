1c5948f6a02ffce29b4d8a693c2bb825d03cc5e5
Gaetano Foster <gaetanox109@gmail.com>
2022-01-13 19:19:03 -0500

    Made new types (Dynamic, Entity) which handle updating and rendering meshes. They can also be rotated.

--- src/engine/Engine.cpp
+++ src/engine/Engine.cpp
@@ -38,3 +38,95 @@ void MultiplyMatrixVector(Vec3D *o, Vec3D i, Mat4x4 m)
         o->z /= w;
     }
 }
+
+void Entity::Update()
+{
+    m_matRotation.Init();
+    
+    m_matRotation.m[0][0] = cosf(zRot) * cosf(yRot);
+    m_matRotation.m[1][0] = cosf(zRot) * sinf(yRot) * sinf(xRot) - sinf(zRot) * cosf(xRot); // row 1
+    m_matRotation.m[2][0] = cosf(zRot) * sinf(yRot) * cosf(xRot) + sinf(zRot) * sinf(xRot);
+    m_matRotation.m[0][1] = sinf(zRot) * cosf(yRot);
+    m_matRotation.m[1][1] = sinf(zRot) * sinf(yRot) * sinf(xRot) + cosf(zRot) * cosf(xRot); // row 2
+    m_matRotation.m[2][1] = sinf(zRot) * sinf(yRot) * cosf(xRot) - cosf(zRot) * sinf(xRot);
+    m_matRotation.m[0][2] = -sinf(yRot);
+    m_matRotation.m[1][2] = cosf(yRot) * sinf(xRot); // row 3
+    m_matRotation.m[2][2] = cosf(yRot) * cosf(xRot);
+}
+
+void Entity::Render(Vec3D vCamera, Mat4x4 matProj, olc::PixelGameEngine *engine)
+{
+    for (auto tri : mesh)
+    {
+        Triangle triProj, triTrans, triRot;
+
+        for (int n = 0; n < 3; n++)
+		    MultiplyMatrixVector(&triRot.p[n], tri.p[n], m_matRotation);
+
+        triTrans = triRot;
+        for (int n = 0; n < 3; n++)// translate the triangle
+        {
+            triTrans.p[n].x = triRot.p[n].x + x;
+            triTrans.p[n].y = triRot.p[n].y + y;
+            triTrans.p[n].z = triRot.p[n].z + z;
+        }
+
+        // Use Cross-Product to Get Surface Normal 
+		Vec3D vNormal, line1, line2;
+		line1.x = triTrans.p[1].x - triTrans.p[0].x;
+		line1.y = triTrans.p[1].y - triTrans.p[0].y;
+		line1.z = triTrans.p[1].z - triTrans.p[0].z;
+
+		line2.x = triTrans.p[2].x - triTrans.p[0].x;
+		line2.y = triTrans.p[2].y - triTrans.p[0].y;
+	    line2.z = triTrans.p[2].z - triTrans.p[0].z;
+
+	    vNormal.x = line1.y * line2.z - line1.z * line2.y;
+	    vNormal.y = line1.z * line2.x - line1.x * line2.z;
+	    vNormal.z = line1.x * line2.y - line1.y * line2.x;
+
+        // it's normally normal to normalize a normal
+	    float l = sqrtf(vNormal.x * vNormal.x + vNormal.y * vNormal.y + vNormal.z * vNormal.z);
+	    vNormal.x /= l;
+        vNormal.y /= l; 
+        vNormal.z /= l;
+        
+        if  (vNormal.x * (triTrans.p[0].x - vCamera.x) +
+             vNormal.y * (triTrans.p[0].y - vCamera.y) +                                        
+             vNormal.z * (triTrans.p[0].z - vCamera.z) < 0) 
+        {
+            triProj = triTrans;
+            for (int n = 0; n < 3; n++) // apply perspective/projection to triangle
+                MultiplyMatrixVector(&triProj.p[n], triTrans.p[n], matProj);
+
+            // Scale mesh into view 
+            triProj.p[0].x += 1.0f; 
+            triProj.p[0].y += 1.0f;
+		    triProj.p[1].x += 1.0f;
+            triProj.p[1].y += 1.0f;
+		    triProj.p[2].x += 1.0f; 
+            triProj.p[2].y += 1.0f;
+	        triProj.p[0].x *= 0.5f * (float)SCREEN_WIDTH;
+		    triProj.p[0].y *= 0.5f * (float)SCREEN_HEIGHT;
+	        triProj.p[1].x *= 0.5f * (float)SCREEN_WIDTH;
+	        triProj.p[1].y *= 0.5f * (float)SCREEN_HEIGHT;
+	        triProj.p[2].x *= 0.5f * (float)SCREEN_WIDTH;
+	        triProj.p[2].y *= 0.5f * (float)SCREEN_HEIGHT;
+
+            // Illumination 
+            Vec3D vLightDir = (Vec3D) { 0.0f, 0.0f, -1.0f }; // spaghetti
+            float ld = sqrtf(vLightDir.x * vLightDir.x + vLightDir.y * vLightDir.y + vLightDir.z * vLightDir.z);
+		    vLightDir.x /= ld; 
+            vLightDir.y /= ld; 
+            vLightDir.z /= ld;
+
+		    // How similar is normal to light direction?
+		    float dp = vNormal.x * vLightDir.x + vNormal.y * vLightDir.y + vNormal.z * vLightDir.z;
+
+            engine->FillTriangle(triProj.p[0].x, triProj.p[0].y,
+                                 triProj.p[1].x, triProj.p[1].y, 
+                                 triProj.p[2].x, triProj.p[2].y,
+                                 olc::Pixel(dp * 255, dp * 255, dp * 255, 255));
+        }
+    }
+}
--- src/engine/Engine.h
+++ src/engine/Engine.h
@@ -1,33 +1,71 @@
 #ifndef ENGINE_H
-    #define ENGINE_H
+#define ENGINE_H
 
-    #include <cmath>
-    #include <vector>
+#include <cmath>
+#include <vector>
+#include "../olcPixelGameEngine.h"
 
-    #define SCREEN_WIDTH        320//1280
-    #define SCREEN_HEIGHT       180//720
-    #define ASPECT_RATIO        (float)(SCREEN_HEIGHT / (float)SCREEN_WIDTH)
-    #define SWAP(x,y) do { (x)=(x)^(y); (y)=(x)^(y); (x)=(x)^(y); } while(0)
+#define SCREEN_WIDTH        640
+#define SCREEN_HEIGHT       360
+#define ASPECT_RATIO        (float)(SCREEN_HEIGHT / (float)SCREEN_WIDTH)
+#define SWAP(x,y) do { (x)=(x)^(y); (y)=(x)^(y); (x)=(x)^(y); } while(0)
 
-    struct Vec3D 
-    {
-        float x, y, z;
-    };
+struct Vec3D 
+{
+    float x, y, z;
+};
 
-    struct Triangle 
-    {
-        Vec3D p[3];
-    };
 
-    struct Mat4x4 
-    {
-        float m[4][4] = { 0 };
-        void Init();
-        void SetProjectionMatrix(float near, float far, float fov);
-    };
+struct Triangle 
+{
+    Vec3D p[3];
+};
 
-    typedef std::vector<Triangle> Mesh;
 
-    void MultiplyMatrixVector(Vec3D *o, Vec3D i, Mat4x4 m);
+struct Mat4x4 
+{
+    float m[4][4] = { 0 };
+    void Init();
+    void SetProjectionMatrix(float near, float far, float fov);
+};
+
+
+class Dynamic
+{
+public:
+    float x = 0, y = 0, z = 1; // to be added to positions in tris
+    float xRot = 0, yRot = 0, zRot = 0;
+public:
+    virtual void Update() = 0;
+};
+
+class Entity : public Dynamic
+{
+public:
+    std::vector<Triangle> mesh;
+public:
+    void Update() override;
+    void Render(Vec3D vCamera, Mat4x4 matProj, olc::PixelGameEngine *engine);
+private:
+    Mat4x4 m_matRotation;
+};
+
+
+class Rasterizer : public olc::PixelGameEngine
+{
+public:
+	Rasterizer();
+
+public:
+	bool OnUserCreate() override;
+	bool OnUserUpdate(float fElapsedTime) override;
+
+private:
+	Mat4x4 matProj;
+    Vec3D vCamera;
+    Entity eCube;
+};
+
+void MultiplyMatrixVector(Vec3D *o, Vec3D i, Mat4x4 m);
 
 #endif
