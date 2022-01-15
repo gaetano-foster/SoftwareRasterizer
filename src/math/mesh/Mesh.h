61ac047df13d7cc27cb457ee9f8e8d024c59dc73
Gaetano Foster <gaetanox109@gmail.com>
2022-01-15 01:03:18 -0500

    Moved Mesh and RasterableTriangle to Mesh.h and Mesh.cc

---
+++ src/math/mesh/Mesh.h
@@ -0,0 +1,19 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include "../Maths.h"
+
+struct Mesh
+{
+    std::vector<Triangle> vecTris;
+
+    bool LoadFromObjectFile(std::string sFileName);
+};
+
+struct RasterableTriangle
+{
+    Triangle tri;
+    olc::Pixel pColor;
+};
+
+#endif
